d3a35bbb87050cfdef516df4148f8dfb5d1e1314
MuhammadAbozaid77 <muhammadaozaid77@gmail.com>
2024-09-07 01:15:57 +0300

    single page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,13 @@
+import ProductImages from "../_components/_features/products/ProductImages";
+import Wrapper from "../_components/ui/Wrapper";
+
 export default function SingelPage() {
-  return <div>SingelPage</div>;
+  return (
+    <Wrapper classesName="relative flex lg:flex-row flex-col gap-16">
+      <div className="lg:w-1/2 w-full lg:sticky top-20 h-max">
+        <ProductImages />
+      </div>
+      <div className="lg:w-1/2 w-full flex flex-col gap-6">gggg</div>
+    </Wrapper>
+  );
 }
---
+++ src/app/_components/_features/products/ProductImages.tsx
@@ -0,0 +1,57 @@
+"use client";
+import Image from "next/image";
+import image1 from "../../../../../public/product1.png";
+import image2 from "../../../../../public/product2.png";
+import image3 from "../../../../../public/paypal.png";
+import { useState } from "react";
+export default function ProductImages() {
+  const images = [
+    {
+      id: 1,
+      pic: image1,
+    },
+    {
+      id: 2,
+      pic: image2,
+    },
+    {
+      id: 3,
+      pic: image3,
+    },
+  ];
+
+  const [imageIndex, setImageIndex] = useState(0);
+
+  return (
+    <>
+      <div>
+        <div className="relative h-[500px]">
+          <Image
+            alt=""
+            src={images[imageIndex].pic}
+            fill
+            sizes="50vw"
+            className="object-cover rounded-md border"
+          />
+        </div>
+        <div className="flex justify-between mt-8 gap-4">
+          {images.map((img, index) => (
+            <div
+              className="relative w-1/4 h-32 mt-8 gap-4 cursor-pointer"
+              key={img.id}
+              onClick={() => setImageIndex(index)}
+            >
+              <Image
+                alt=""
+                src={img.pic}
+                fill
+                sizes="30vw"
+                className="object-cover rounded-md border"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
